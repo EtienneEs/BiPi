e1b27c6df09b53305bcf9b24f8f5909c605d310e
Etienne Schmelzer <etienne.schmelzer@gmx.net>
2021-05-10 16:09:28 +0200

    Add price model to products

--- bipi/products/models.py
+++ bipi/products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from model_utils.models import TimeStampedModel
+from djmoney.models.fields import MoneyField
+from ..accounts.models import Organization
 
 
 class Product(TimeStampedModel):
@@ -16,3 +18,29 @@ class Product(TimeStampedModel):
 
     def get_absolute_url(self):
         return reverse('products:product_detail', kwargs={'pk': self.pk})
+
+
+class Price(TimeStampedModel):
+    product = models.ForeignKey(
+        Product,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE
+    )
+    organization = models.ForeignKey(
+        Organization,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE
+    )
+    price = MoneyField(
+        max_digits=14,
+        decimal_places=2,
+        default_currency='EUR'
+    )
+
+    class Meta:
+        unique_together = (('product', 'organization'), )
+
+    def __str__(self):
+        return f'{self.organization} - {self.product} {self.price}'
